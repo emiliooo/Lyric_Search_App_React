d34657892fe4319d4a4a55c9b30e545c821b6519
Emil <emilburaczynski@gmail.com>
2019-04-18 11:28:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
+import {Provider} from './context';
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar/> 
+          <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+          </div>
+          </React.Fragment> 
+        </Router>
+      </Provider>
     );
   }
 }
